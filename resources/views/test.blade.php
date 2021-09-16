c68b8e10120343f605012e9058ce219df013e726
fougmarc <fougmarc@gmail.com>
2021-09-16 17:07:41 +0000

    finalisation

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -7,48 +7,46 @@
             <div class="card">
                 <div class="card-header">{{ __('Documents') }}</div>
 
-                <div class="card-body">
+            
+            
                     <table class="table">
-                        <thead>
-                            <tr>
-                                <th>nom</th>
-                                <th>type</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($produits as $produit)
-                           
-                            <tr>
-                            <form method="POST" action="{{ url('validerdoc/'.$produit->id) }}">
-                            @csrf
-                            <th > </th>
-                                <th > {{  $produit->nom }}</th>
-                                <th >{{  $produit->type }}</th>
-                                
-                                <th>
-                                    @can('view',$produit)
-                                    <button class="btn btn-secondary"> voir </button>
-                                    @endcan
-                                   
-                                </th>
-                                <th> 
-                                    
-                                <button type="submit" class="btn btn-secondary">Valider</button>
-                            
-                                </th>
-                            </form> 
-                            </tr>
-                           
-                        @endforeach
-                        </tbody>
-                        
+                    <thead>
+                        <tr>
                        
+                        <th scope="col">nom</th>
+                        <th scope="col">type</th>
+                        
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($produits as $produit)
+                    @csrf
+                    <form method="POST" action="{{ url('validerdoc/'.$produit->id) }}">
+                        <tr>
+                       
+                        <td>{{  $produit->nom }}</td>
+                        <td>{{  $produit->type }}</td>
+                        @if($produit->profile == auth()->user()->profile)
+                        <td>
+                            <button class="btn btn-info"> editer </button>
+                        </td>
+                        <td>
+                            <button type="submit" class="btn btn-success">Valider</button>
+                        </td>
+
+                        <td>
+                            <button class="btn btn-danger"> Supprimer </button>  
+                        </td>                             
+                        @endif
+                        </tr>
+                    </form> 
+                    @endforeach 
+                    </tbody>
                     </table>
-                 
-                  
+                                    
                 </div>
             </div>
         </div>
-    </div>
+   
 </div>
 @endsection
